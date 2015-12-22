a5c37023f37e7adfcfcd649528f814b13f0a287e
Victor <victor.xiong24@gmail.com>
2015-12-21 23:08:25 -0500

    add grunt watch and eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
-		pkgFile: 'package.json'
+		pkgFile: 'package.json',
+		eslint: {
+			target: [
+				'lib/*.js',
+				'src/*.js',
+				'test/*.js',
+				'Gruntfile.js'
+			]
+		},
+		watch: {
+			files: ['**/*.js'],
+			tasks: ['eslint']
+		}
 	});
+
+	grunt.registerTask('default', ['eslint']);
+
+	require('load-grunt-tasks')(grunt);
 };
