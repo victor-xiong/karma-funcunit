fb1ff9b08b7384b88b87cd099055b9a2f4a1f255
Victor <victor.xiong24@gmail.com>
2016-02-09 21:24:18 -0500

    enable 4 spaces indention rule in eslint and convert tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,21 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkgFile: 'package.json',
-		eslint: {
-			target: [
-				'lib/*.js',
-				'src/*.js',
-				'test/*.js',
-				'Gruntfile.js'
-			]
-		},
-		watch: {
-			files: ['**/*.js'],
-			tasks: ['eslint']
-		}
-	});
+    grunt.initConfig({
+        pkgFile: 'package.json',
+        eslint: {
+            target: [
+                'lib/*.js',
+                'src/*.js',
+                'test/*.js',
+                'Gruntfile.js'
+            ]
+        },
+        watch: {
+            files: ['**/*.js'],
+            tasks: ['eslint']
+        }
+    });
 
-	grunt.registerTask('default', ['eslint']);
+    grunt.registerTask('default', ['eslint']);
 
-	require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 };
